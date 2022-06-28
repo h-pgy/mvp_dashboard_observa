c598be1b7242c788a08dc35d2364a8ea91256d2b
h-pgy <hpougy@gmail.com>
2022-06-28 20:37:11 -0300

    indicadores selecionados marilia e jacques

--- core/config.py
+++ core/config.py
@@ -11,27 +11,10 @@ FOLDER_DISTRITOS = solve_path('SIRGAS_SHP_distrito', FOLDER_MAPS)
 
 APP_DATA_FOLDER = solve_path('app', 'data')
 
-distorcao_idade_ano = [
-    '04.01.03 Taxa de distorção da idade-ano para o ano no Ensino Fundamental nos anos iniciais (%)',
-   '04.01.04 Taxa de distorção da idade-ano para o ano no Ensino Fundamental nos anos finais (%)',
+LIST_INDICADORES = [
+    'População total',
+    'População total - homens',
+    'População total - mulheres',
+    'Índice de envelhecimento',
+    'Razão de sexos'
 ]
-
-raca_alunos = [
-    'Alunos da rede municipal de ensino da raça/cor amarela (%)',
-    'Alunos da rede municipal de ensino da raça/cor branca (%)',
-    'Alunos da rede municipal de ensino da raça/cor indígena (%)',
-    'Alunos da rede municipal de ensino da raça/cor parda (%)',
-    'Alunos da rede municipal de ensino da raça/cor preta (%)',
-       
-       ]
-
-abandono = [
-    'Taxa de abandono escolar no Ensino Fundamental da rede municipal (%)',
-   'Taxa de abandono escolar no Ensino Médio da rede municipal (%)',
-]
-
-
-LIST_INDICADORES = []
-LIST_INDICADORES.extend(distorcao_idade_ano)
-LIST_INDICADORES.extend(raca_alunos)
-LIST_INDICADORES.extend(abandono)
